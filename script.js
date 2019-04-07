70d8d5df891c299d4c17c410ef1bb6673d1a3ea8
Tiffany Ray <tiffanyraylee@gmail.com>
2019-04-07 11:47:13 -0500

    final product, quick project

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(() => {
-    let button = $('.button');
-    let menu = $('.menu');
+    let button = $('#mobile-menu');
+    let menu = $('.nav-ul');
+    let lis = $('.nav-li');
 
     button.click( function() {
         console.log('hello');
 
         menu.toggleClass('open');
+        lis.toggleClass('open');
     });
+
+
 })
